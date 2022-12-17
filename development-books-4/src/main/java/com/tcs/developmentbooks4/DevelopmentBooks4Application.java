050ceded77329c4fb0e2f877aa3be8af99e498c1
HussainLuqmanji <hluqmanji@gmail.com>
2022-12-18 01:12:23 +0530

    chore: create springboot project for development books.

---
+++ development-books-4/src/main/java/com/tcs/developmentbooks4/DevelopmentBooks4Application.java
@@ -0,0 +1,13 @@
+package com.tcs.developmentbooks4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopmentBooks4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelopmentBooks4Application.class, args);
+	}
+
+}
