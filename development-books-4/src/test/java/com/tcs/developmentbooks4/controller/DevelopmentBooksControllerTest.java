f3e810d7eee74d48796425d0a63bff64f7cdc846
HussainLuqmanji <hluqmanji@gmail.com>
2022-12-18 01:28:38 +0530

    refactor: added Before each method to create controller object for test cases.

--- development-books-4/src/test/java/com/tcs/developmentbooks4/controller/DevelopmentBooksControllerTest.java
+++ development-books-4/src/test/java/com/tcs/developmentbooks4/controller/DevelopmentBooksControllerTest.java
@@ -6,27 +6,33 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class DevelopmentBooksControllerTest {
 
+	DevelopmentBooksController controller;
+
+	@BeforeEach
+	public void setUp() {
+		controller = new DevelopmentBooksController();
+	}
+
 	@Test
 	void getBookNameShouldReturnNameOfTheBook() {
-		DevelopmentBooksController controller = new DevelopmentBooksController();
 		List<String> result = controller.getAllBooks();
 		assertEquals("Clean Code", result.get(0));
 	}
 
-    @Test
-    public void getAllBooksShouldReturnFiveBookNames() {
-        DevelopmentBooksController controller = new DevelopmentBooksController();
-        List<String> books = controller.getAllBooks();
-        assertEquals(5, books.size());
-        assertTrue(books.containsAll(getAllBooks()));
-    }
+	@Test
+	public void getAllBooksShouldReturnFiveBookNames() {
+		List<String> books = controller.getAllBooks();
+		assertEquals(5, books.size());
+		assertTrue(books.containsAll(getAllBooks()));
+	}
 
-    private List<String> getAllBooks() {
-        return Arrays.asList("Clean Code", "The Clean Coder", "Clean Architecture",
-                "Test Driven Development by Example", "Working Effectively With Legacy Code");
-    }
+	private List<String> getAllBooks() {
+		return Arrays.asList("Clean Code", "The Clean Coder", "Clean Architecture",
+				"Test Driven Development by Example", "Working Effectively With Legacy Code");
+	}
 }
