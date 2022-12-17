62ec2309bc77f7e0e8c216baaab87c4f423122e7
HussainLuqmanji <hluqmanji@gmail.com>
2022-12-18 01:49:17 +0530

    refactor: added enum for book names.

--- development-books-4/src/main/java/com/tcs/developmentbooks4/service/BookService.java
+++ development-books-4/src/main/java/com/tcs/developmentbooks4/service/BookService.java
@@ -1,15 +1,17 @@
 package com.tcs.developmentbooks4.service;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.springframework.stereotype.Service;
+
+import com.tcs.developmentbooks4.enums.BooksEnum;
 
 @Service
 public class BookService {
 
 	public List<String> getAllBooks() {
-		return Arrays.asList("Clean Code", "The Clean Coder", "Clean Architecture",
-				"Test Driven Development by Example", "Working Effectively With Legacy Code");
+        return Stream.of(BooksEnum.values()).map(Enum::name).collect(Collectors.toList());
 	}
 }
